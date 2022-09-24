39803e5f8ebc51c1d265a5aa9c95884f4dce2340
mylistrikapp@gmail.com <bimataruna21@gmail.com>
2022-09-24 14:13:51 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ForgotPassword from "./Components/ForgotPassword";
-import reportWebVitals from "./reportWebVitals";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 
@@ -31,8 +30,3 @@ ReactDOM.render(
   </AlertProvider>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
